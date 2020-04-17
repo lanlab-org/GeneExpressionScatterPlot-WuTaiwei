670944f5492e37bd3abe73855cca9f8e5d7c29a4
TieWay59 <tieway59@foxmail.com>
2020-04-17 14:46:04 +0800

    调整 代码缩进和整体格式 新增 render函数抽离重复代码 调整 MyQuery.js 减少多余代码

--- js/MyQuery.js
+++ js/MyQuery.js
@@ -6,93 +6,80 @@ let d2name;
 let infoname;
 
 $(document).ready(function () {
-    $('#raButton').click(function () {
-        if(this.getAttribute("value")==="隐藏相关系数"){
-            this.setAttribute("value","显示相关系数");
-            $('#idOfRelat').css("display","none");
-        }else{
-            this.setAttribute("value","隐藏相关系数");
-            $('#idOfRelat').css("display","block");
-        }
-    });
-    $('#ioButton').click(function () {
-        if(this.getAttribute("value")==="隐藏单点信息"){
-            this.setAttribute("value","显示单点信息");
-            $('#idOfInfo').css("display","none");
-        }else{
-            this.setAttribute("value","隐藏单点信息");
-            $('#idOfInfo').css("display","block");
-        }
-    });
-    $("#data1").change(function () {
-        //console.log("Yes");
-        const filePath = $(this).val();
-        if (filePath.indexOf("json") !== -1) {
-            const arr = filePath.split('\\');
-            const fileName = arr[arr.length - 1];
-            d1name = fileName;
-            check1 = 1;
-            const idp=$("#data1_Button");
-            idp.css("background-color", "rgba(100,200,100,0.05)");
-            idp.css("color", "rgba(100,200,100,1.00)");
-            idp.css("border-color", "rgba(100,200,100,1.00)");
-            idp.html(fileName);
-        } else {
-            check1 = -1;
-            const idp=$('#data2_Button');
-            idp.html("未选择或类型有误！");
-            idp.css("background-color", "rgba(200,100,100,0.05)");
-            idp.css("color", "rgba(200,100,100,1.00)");
-            idp.css("border-color", "rgba(200,100,100,1.00)");
-            return false;
-        }
-    });
-    $("#data2").change(function () {
-        //console.log("Yes");
-        const filePath = $(this).val();
-        if (filePath.indexOf("json") !== -1) {
-            const arr = filePath.split('\\');
-            const fileName = arr[arr.length - 1];
-            d2name = fileName;
-            check2 = 1;
-            const idp=$("#data2_Button");
-            idp.css("background-color", "rgba(100,200,100,0.05)");
-            idp.css("color", "rgba(100,200,100,1.00)");
-            idp.css("border-color", "rgba(100,200,100,1.00)");
-            idp.html(fileName);
-        } else {
-            check2 = -1;
+  $('#raButton').click(function () {
+    if (this.getAttribute("value") === "隐藏相关系数") {
+      this.setAttribute("value", "显示相关系数");
+      $('#idOfRelat').css("display", "none");
+    } else {
+      this.setAttribute("value", "隐藏相关系数");
+      $('#idOfRelat').css("display", "block");
+    }
+  });
+  $('#ioButton').click(function () {
+    if (this.getAttribute("value") === "隐藏单点信息") {
+      this.setAttribute("value", "显示单点信息");
+      $('#idOfInfo').css("display", "none");
+    } else {
+      this.setAttribute("value", "隐藏单点信息");
+      $('#idOfInfo').css("display", "block");
+    }
+  });
 
-            const idp=$("#data2_Button");
-            idp.html("未选择或类型有误！");
-            idp.css("background-color", "rgba(200,100,100,0.05)");
-            idp.css("color", "rgba(200,100,100,1.00)");
-            idp.css("border-color", "rgba(200,100,100,1.00)");
-            return false;
-        }
-    });
-    $("#data3").change(function () {
-        //console.log("Yes");
-        const filePath = $(this).val();
-        if (filePath.indexOf("json") !== -1) {
-            const arr = filePath.split('\\');
-            const fileName = arr[arr.length - 1];
-            infoname = fileName;
-            check3 = 1;
-            const idp=$("#data3_Button");
-            idp.css("background-color", "rgba(100,200,100,0.05)");
-            idp.css("color", "rgba(100,200,100,1.00)");
-            idp.css("border-color", "rgba(100,200,100,1.00)");
-            idp.html(fileName);
-        } else {
-            if(filePath.length>=0)check3 = -1;
-            else check3 = 0;
-            const idp=$("#data3_Button");
-            idp.html("未选择或类型有误！");
-            idp.css("background-color", "rgba(200,100,100,0.05)");
-            idp.css("color", "rgba(200,100,100,1.00)");
-            idp.css("border-color", "rgba(200,100,100,1.00)");
-            return false;
-        }
-    });
+  function renderValid(obj, str) {
+    obj.css("background-color", "rgba(100,200,100,0.05)");
+    obj.css("color", "rgba(100,200,100,1.00)");
+    obj.css("border-color", "rgba(100,200,100,1.00)");
+    obj.html(str);
+  }
+
+  function renderInvalid(obj) {
+    obj.css("background-color", "rgba(200,100,100,0.05)");
+    obj.css("color", "rgba(200,100,100,1.00)");
+    obj.css("border-color", "rgba(200,100,100,1.00)");
+    obj.html("未选择或类型有误！");
+  }
+
+  $("#data1").change(function () {
+    //console.log("Yes");
+    const filePath = $(this).val();
+    if (filePath.indexOf("json") !== -1) {
+      const arr = filePath.split('\\');
+      d1name = arr[arr.length - 1];
+      check1 = 1;
+      renderValid($("#data1_Button"), d1name);
+    } else {
+      check1 = -1;
+      renderInvalid($('#data2_Button'));
+      return false;
+    }
+  });
+  $("#data2").change(function () {
+    //console.log("Yes");
+    const filePath = $(this).val();
+    if (filePath.indexOf("json") !== -1) {
+      const arr = filePath.split('\\');
+      d2name = arr[arr.length - 1];
+      check2 = 1;
+      renderValid($("#data2_Button"), d2name);
+    } else {
+      check2 = -1;
+      renderInvalid($("#data2_Button"));
+      return false;
+    }
+  });
+  $("#data3").change(function () {
+    //console.log("Yes");
+    const filePath = $(this).val();
+    if (filePath.indexOf("json") !== -1) {
+      const arr = filePath.split('\\');
+      infoname = arr[arr.length - 1];
+      check3 = 1;
+      renderValid($("#data3_Button"), infoname);
+    } else {
+      if (filePath.length >= 0) check3 = -1;
+      else check3 = 0;
+      renderInvalid($("#data3_Button"));
+      return false;
+    }
+  });
 });
